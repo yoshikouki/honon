fd8ceaf01901bec38c7dd02aad2a9c95ef035f48
yoshikouki <yoshikouki@gmail.com>
2024-04-30 06:11:02 +0900

    Use Bun as JS runtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1 className="font-black text-6xl">
-        HonoN
-      </h1>
+      <h1 className="font-black text-6xl">HonoN</h1>
     </main>
   );
 }
