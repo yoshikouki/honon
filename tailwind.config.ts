9aba134dc6c21ba148ae7a4bbfb81e29c74b59f1
yoshikouki <yoshikouki@gmail.com>
2024-06-11 09:07:39 +0900

    Update theme config of TailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,96 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx}"],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "swipe-x": {
+          "0%, 100%": { transform: "translateX(0)" },
+          "33%": { transform: "translateX(77%)" },
+          "66%": { transform: "translateX(-77%)" },
+        },
+        rotate: {
+          "0%": { transform: "rotate(0deg)" },
+          "20%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(90deg)" },
+          "45%": { transform: "rotate(90deg)" },
+          "50%": { transform: "rotate(180deg)" },
+          "70%": { transform: "rotate(180deg)" },
+          "75%": { transform: "rotate(270deg)" },
+          "95%": { transform: "rotate(270deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        touch: {
+          "40%": { transform: "scale(1)" },
+          "70%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "swipe-x": "swipe-x 3s infinite",
+        rotate: "rotate 4s infinite ease-out",
+        touch: "touch 1s infinite ease-out",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -16,5 +99,6 @@ const config: Config = {
     },
   },
   plugins: [],
-};
+} satisfies Config;
+
 export default config;
